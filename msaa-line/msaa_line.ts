bc9eb4eb9841d3a8ae187c37c5a3bd83e2be4dd0
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-02-12 11:38:59 +0100

    clean up

--- msaa-line/msaa_line.ts
+++ msaa-line/msaa_line.ts
@@ -67,70 +67,6 @@ function createBundle(
   return encoder.finish();
 }
 
-async function init(
-  device: GPUDevice,
-  dimensions: Dimensions,
-  sampleCount: number,
-): Promise<void> {
-  const format = "rgba8unorm-srgb";
-
-  const shader = device.createShaderModule({
-    code: await Deno.readTextFile("./shader.wgsl"),
-  });
-
-  const pipelineLayout = device.createPipelineLayout({
-    bindGroupLayouts: [],
-  });
-
-  const multisampledBuffer = device.createTexture({
-    size: dimensions,
-    sampleCount,
-    format,
-    usage: 0x10,
-  }).createView();
-
-  const vertexCount = 50;
-  const vertexSize = 6;
-  const vertexData = new Float32Array((vertexCount * 2) * vertexSize);
-  for (let i = 0; i < vertexData.byteLength; i += (vertexSize * 2)) {
-    const percent = i / vertexCount;
-    const sin = Math.sin(percent * 2 * Math.PI);
-    const cos = Math.cos(percent * 2 * Math.PI);
-
-    vertexData[i] = 0; // x
-    vertexData[i + 1] = 0; // y
-    vertexData[i + 2] = 1; // r
-    vertexData[i + 3] = -sin; // g
-    vertexData[i + 4] = cos; // b
-    vertexData[i + 5] = 1; // a
-
-    vertexData[i + 6] = cos; // x
-    vertexData[i + 7] = sin; // y
-    vertexData[i + 8] = sin; // r
-    vertexData[i + 9] = -cos; // g
-    vertexData[i + 10] = 1; // b
-    vertexData[i + 11] = 1; // a
-  }
-
-  const vertexBuffer = createBufferInit(device, {
-    label: "Vertex Buffer",
-    usage: 0x0020,
-    contents: vertexData.buffer,
-  });
-
-  const bundle = createBundle(
-    device,
-    format,
-    shader,
-    pipelineLayout,
-    sampleCount,
-    vertexBuffer,
-    vertexCount * 2,
-  );
-
-  await render(device, dimensions, multisampledBuffer, bundle);
-}
-
 async function render(
   device: GPUDevice,
   dimensions: Dimensions,
@@ -164,12 +100,70 @@ const dimensions: Dimensions = {
   width: 1600,
   height: 1200,
 };
+const sampleCount = 4;
 
 const adapter = await navigator.gpu.requestAdapter();
 const device = await adapter?.requestDevice();
 
 if (!device) {
   console.error("no suitable adapter found");
-} else {
-  await init(device, dimensions, 4);
+  Deno.exit(0);
 }
+
+const format = "rgba8unorm-srgb";
+
+const shader = device.createShaderModule({
+  code: await Deno.readTextFile("./shader.wgsl"),
+});
+
+const pipelineLayout = device.createPipelineLayout({
+  bindGroupLayouts: [],
+});
+
+const multisampledBuffer = device.createTexture({
+  size: dimensions,
+  sampleCount,
+  format,
+  usage: 0x10,
+}).createView();
+
+const vertexCount = 50;
+const vertexSize = 6;
+const vertexData = new Float32Array((vertexCount * 2) * vertexSize);
+for (let i = 0; i < vertexData.byteLength; i += (vertexSize * 2)) {
+  const percent = i / vertexCount;
+  const sin = Math.sin(percent * 2 * Math.PI);
+  const cos = Math.cos(percent * 2 * Math.PI);
+
+  vertexData[i] = 0; // x
+  vertexData[i + 1] = 0; // y
+  vertexData[i + 2] = 1; // r
+  vertexData[i + 3] = -sin; // g
+  vertexData[i + 4] = cos; // b
+  vertexData[i + 5] = 1; // a
+
+  vertexData[i + 6] = cos; // x
+  vertexData[i + 7] = sin; // y
+  vertexData[i + 8] = sin; // r
+  vertexData[i + 9] = -cos; // g
+  vertexData[i + 10] = 1; // b
+  vertexData[i + 11] = 1; // a
+}
+
+const vertexBuffer = createBufferInit(device, {
+  label: "Vertex Buffer",
+  usage: 0x0020,
+  contents: vertexData.buffer,
+});
+
+const bundle = createBundle(
+  device,
+  format,
+  shader,
+  pipelineLayout,
+  sampleCount,
+  vertexBuffer,
+  vertexCount * 2,
+);
+
+await render(device, dimensions, multisampledBuffer, bundle);
