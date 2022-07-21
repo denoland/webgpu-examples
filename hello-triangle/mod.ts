37955f7ca3f7d5adf78b8f07a4c16bce627d7bb2
crowlkats <crowlkats@toaxl.com>
2022-07-21 15:24:40 +0200

    hello-triangle

--- hello-triangle/mod.ts
+++ hello-triangle/mod.ts
@@ -19,15 +19,15 @@ if (!device) {
 }
 
 const shaderCode = `
-[[stage(vertex)]]
-fn vs_main([[builtin(vertex_index)]] in_vertex_index: u32) -> [[builtin(position)]] vec4<f32> {
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
     let x = f32(i32(in_vertex_index) - 1);
     let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
     return vec4<f32>(x, y, 0.0, 1.0);
 }
 
-[[stage(fragment)]]
-fn fs_main() -> [[location(0)]] vec4<f32> {
+@fragment
+fn fs_main() -> @location(0) vec4<f32> {
     return vec4<f32>(1.0, 0.0, 0.0, 1.0);
 }
 `;
@@ -65,13 +65,14 @@ const renderPass = encoder.beginRenderPass({
     {
       view: texture.createView(),
       storeOp: "store",
-      loadValue: [0, 1, 0, 1],
+      loadOp: "clear",
+      clearValue: [0, 1, 0, 1],
     },
   ],
 });
 renderPass.setPipeline(renderPipeline);
 renderPass.draw(3, 1);
-renderPass.endPass();
+renderPass.end();
 
 copyToBuffer(encoder, texture, outputBuffer, dimensions);
 
