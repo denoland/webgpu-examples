6c505182170ba10cc7bfe86203477688b96648bb
tdillon <tdillon@users.noreply.github.com>
2022-11-15 20:24:37 -0500

    fix: pass map argument to parseInt for command line usage (#6)

--- hello-compute/mod.ts
+++ hello-compute/mod.ts
@@ -5,7 +5,7 @@ const OVERFLOW = 0xffffffff;
 // Get some numbers from the command line, or use the default 1, 4, 3, 295.
 let numbers: Uint32Array;
 if (Deno.args.length > 0) {
-  numbers = new Uint32Array(Deno.args.map(parseInt));
+  numbers = new Uint32Array(Deno.args.map(a => parseInt(a)));
 } else {
   numbers = new Uint32Array([1, 4, 3, 295]);
 }
